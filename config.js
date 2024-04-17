2d3994527fb3b7f4c193653d70624a0607a539a0
witherlands <167359805+witherlands@users.noreply.github.com>
2024-04-17 13:03:34 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "14gg6GodbIx9a3JA7HGWyjBypgm1dA49rCFktECNXs5s";
 
 
 /* ------------------------------------------------------------------- */
